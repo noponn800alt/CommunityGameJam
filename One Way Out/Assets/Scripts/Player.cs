b37b8c2db0caa7c0b25a6625635ff0692aeb6e3f
Jaroslaw Glowka <jmglowkaa@gmail.com>
2019-08-26 16:36:37 +0200

    Player Movement

---
+++ One Way Out/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    readonly float runSpeed = 2f;
+    readonly float jumpHeight = 2f;
+
+    bool grounded;
+    float yVelocity;
+
+    Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+
+        yVelocity = 0;
+        grounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(body.velocity.y == 0f)
+        {
+            grounded = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            body.velocity += jumpHeight * Vector3.up;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector3(
+            Mathf.Lerp(0, Input.GetAxis("Horizontal") * runSpeed, 0.8f),
+            body.velocity.y,
+            Mathf.Lerp(0, Input.GetAxis("Vertical") * runSpeed, 0.8f)
+        );
+    }
+}
