ee6c865687b7e8938b11d0c463d58b27b4e5907c
Jaroslaw Glowka <jmglowkaa@gmail.com>
2019-08-26 22:21:16 +0200

    new movement
    I made the movement as you all wanted.

--- One Way Out/Assets/Scripts/Player.cs
+++ One Way Out/Assets/Scripts/Player.cs
@@ -7,6 +7,12 @@ public class Player : MonoBehaviour
     readonly float runSpeed = 2f;
     readonly float jumpHeight = 2f;
 
+    float horizontal;
+    float vertical;
+
+    bool onHorizontal;
+    bool onVertical;
+
     bool grounded;
     float yVelocity;
 
@@ -24,7 +30,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(body.velocity.y == 0f)
+        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("Going right or left");
+            this.onHorizontal = true;
+            this.onVertical = false;
+        }
+         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("going up or down");
+            this.onVertical = true;
+            this.onHorizontal = false;
+        }
+
+        if (body.velocity.y == 0f)
         {
             grounded = true;
         }
@@ -37,10 +56,21 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (onHorizontal)
+        {
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = 0f;
+        }
+        else if (onVertical)
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = 0f;
+        }
+
         body.velocity = new Vector3(
-            Mathf.Lerp(0, Input.GetAxis("Horizontal") * runSpeed, 0.8f),
+            Mathf.Lerp(0, this.horizontal * runSpeed, 0.8f),
             body.velocity.y,
-            Mathf.Lerp(0, Input.GetAxis("Vertical") * runSpeed, 0.8f)
+            Mathf.Lerp(0, this.vertical * runSpeed, 0.8f)
         );
     }
 }
